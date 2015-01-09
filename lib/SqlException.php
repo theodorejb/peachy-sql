f62858ea6915e5f46373dd53fd5bfae9d7e9b90c
Theodore Brown <theodorejb@outlook.com>
2015-01-09 16:21:41 -0600

    SqlException code should match SQL error number

--- lib/SqlException.php
+++ lib/SqlException.php
@@ -13,16 +13,22 @@ class SqlException extends \Exception
     private $params;
 
     /**
-     * @param string     $msg    The error message
-     * @param array      $errors An error array returned by sqlsrv_errors() or mysqli::$error_list)
-     * @param string     $query  The failed query
-     * @param array      $params Any parameters bound to the failed query
-     * @param \Exception $prev   Optionally set a previous exception
-     * @param int        $code   Optionally set an error code
+     * @param string $msg The error message
+     * @param array $errors An error array returned by sqlsrv_errors() or mysqli::$error_list)
+     * @param string $query The failed query
+     * @param array $params Any parameters bound to the failed query
      */
-    public function __construct($msg, array $errors, $query = null, array $params = null, \Exception $prev = null, $code = 0)
+    public function __construct($msg, array $errors, $query = null, array $params = null)
     {
-        parent::__construct($msg, $code, $prev);
+        if (isset($errors[0]['errno'])) {
+            $code = $errors[0]['errno']; // MySQL
+        } elseif (isset($errors[0]['code'])) {
+            $code = $errors[0]['code']; // SQL Server
+        } else {
+            $code = 0;
+        }
+
+        parent::__construct($msg, $code);
 
         $this->errors = $errors;
         $this->query = $query;
