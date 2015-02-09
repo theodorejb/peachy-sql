01bf65c0e375c3bf23f068948d3871010063842b
Theodore Brown <theodorejb@outlook.com>
2015-02-08 23:11:48 -0600

    Use single quotes in test config file

--- test/config.php
+++ test/config.php
@@ -4,22 +4,22 @@
 // this directory which returns a subset of or replacement for the following array.
 
 return [
-    "testWith" => [
-        "mysql"  => true,
-        "sqlsrv" => false, // don't test by default since it doesn't run on Travis
+    'testWith' => [
+        'mysql'  => true,
+        'sqlsrv' => false, // don't test by default since it doesn't run on Travis
     ],
-    "db" => [
-        "mysql" => [
-            "host"     => "127.0.0.1",
-            "username" => "root",
-            "password" => "",
-            "database" => "PeachySQL",
+    'db' => [
+        'mysql' => [
+            'host'     => '127.0.0.1',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'PeachySQL',
         ],
-        "sqlsrv" => [
-            "serverName"     => "Computer-Name\SQLEXPRESS",
-            "connectionInfo" => [
-                "Database"             => "PeachySQL",
-                "ReturnDatesAsStrings" => true,
+        'sqlsrv' => [
+            'serverName'     => 'Computer-Name\SQLEXPRESS',
+            'connectionInfo' => [
+                'Database'             => 'PeachySQL',
+                'ReturnDatesAsStrings' => true,
             ],
         ],
     ],
